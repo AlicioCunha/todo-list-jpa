99d70448b5e287299724f7cc345fe48c9c84401f
alicio.fcn@gmail.com <alicio.fcn@gmail.com>
2020-11-21 23:38:47 -0300

    Criado model de Local

---
+++ src/main/java/br/com/todolist/model/Local.java
@@ -0,0 +1,18 @@
+package br.com.todolist.model;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+public class Local implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "nome", nullable = false, length = 50)
+    private String nome;
+}
