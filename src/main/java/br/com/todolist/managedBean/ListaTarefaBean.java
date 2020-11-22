b3aaa7c9e6de2c267fd617eab0ee28a70a82e430
alicio.fcn@gmail.com <alicio.fcn@gmail.com>
2020-11-22 12:04:56 -0300

    implementacao metodo criar nova tarefa

--- src/main/java/br/com/todolist/managedBean/ListaTarefaBean.java
+++ src/main/java/br/com/todolist/managedBean/ListaTarefaBean.java
@@ -1,8 +1,9 @@
 package br.com.todolist.managedBean;
 
-
 import br.com.todolist.model.ListaTarefa;
+import br.com.todolist.model.util.StatusTarefa;
 import br.com.todolist.repository.ListaTarefaRepository;
+import br.com.todolist.util.FacesMessages;
 
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
@@ -19,13 +20,42 @@ public class ListaTarefaBean implements Serializable {
     @Inject
     private ListaTarefaRepository repository;
 
+    @Inject
+    private FacesMessages messages;
+
     private List<ListaTarefa> listaTarefas;
 
-    public void Consultar() {
+    private ListaTarefa novaTarefa;
+
+    public void consultar() {
         listaTarefas = repository.listarTudo();
     }
 
+    public void criarNovaTarefa() {
+        novaTarefa = new ListaTarefa();
+    }
+
+    public void salvarNovaTarefa(){
+        repository.salvar(novaTarefa);
+        consultar();
+
+        messages.info("Empresa salva com sucesso!");
+    }
+
+
     public List<ListaTarefa> getListaTarefas() {
         return listaTarefas;
+    }
+
+    public StatusTarefa[] getStatusTarefas() {
+        return StatusTarefa.values();
+    }
+
+    public ListaTarefa getNovaTarefa() {
+        return novaTarefa;
+    }
+
+    public void setNovaTarefa(ListaTarefa novaTarefa) {
+        this.novaTarefa = novaTarefa;
     }
 }
