ac6ecf247a5073632fdad172037d3daf011e78df
alicio.fcn@gmail.com <alicio.fcn@gmail.com>
2020-11-22 00:08:43 -0300

    Mapeand usuario como entity

--- src/main/java/br/com/todolist/model/Usuario.java
+++ src/main/java/br/com/todolist/model/Usuario.java
@@ -1,10 +1,12 @@
 package br.com.todolist.model;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.io.Serializable;
 
+@Entity(name = "usuario")
 public class Usuario implements Serializable {
 
     private static final long serialVersionUID = 1L;
