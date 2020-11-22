9ca10c6b15bc880b96cc3d763c3c9e7dbe398627
alicio.fcn@gmail.com <alicio.fcn@gmail.com>
2020-11-22 00:10:29 -0300

    Mapeand listaTarefas como entity

--- src/main/java/br/com/todolist/model/ListaTarefa.java
+++ src/main/java/br/com/todolist/model/ListaTarefa.java
@@ -1,18 +1,13 @@
 package br.com.todolist.model;
 
 
+import br.com.todolist.model.util.StatusTarefa;
+
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-
+@Entity(name = "lista_tarefa")
 public class ListaTarefa implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -39,6 +34,89 @@ public class ListaTarefa implements Serializable {
     @Column(name = "observacao", length = 120)
     private String observacao;
 
+    @Enumerated(EnumType.ORDINAL)
+    @Column(name = "status_tarefa")
+    private StatusTarefa statusTarefa;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Date getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(Date dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+    public Date getDataExecucao() {
+        return dataExecucao;
+    }
+
+    public void setDataExecucao(Date dataExecucao) {
+        this.dataExecucao = dataExecucao;
+    }
+
+    public Date getDataConclusao() {
+        return dataConclusao;
+    }
+
+    public void setDataConclusao(Date dataConclusao) {
+        this.dataConclusao = dataConclusao;
+    }
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+
+    public StatusTarefa getStatusTarefa() {
+        return statusTarefa;
+    }
+
+    public void setStatusTarefa(StatusTarefa statusTarefa) {
+        this.statusTarefa = statusTarefa;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ListaTarefa other = (ListaTarefa) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
 
 }
