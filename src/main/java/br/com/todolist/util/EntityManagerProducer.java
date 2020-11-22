285bd3bdf25d54c30d331be99c901fe6b1efcd47
alicio.fcn@gmail.com <alicio.fcn@gmail.com>
2020-11-21 23:08:29 -0300

    Ajustes de configuracaoi

--- src/main/java/br/com/todolist/util/EntityManagerProducer.java
+++ src/main/java/br/com/todolist/util/EntityManagerProducer.java
@@ -14,7 +14,7 @@ public class EntityManagerProducer {
     private EntityManagerFactory factory;
 
     public EntityManagerProducer() {
-        this.factory = Persistence.createEntityManagerFactory("AgendaPU");
+        this.factory = Persistence.createEntityManagerFactory("TodoListPU");
     }
 
     @Produces
