a805547dabf6fca057bf17093df8eb044c64f576
alicio.fcn@gmail.com <alicio.fcn@gmail.com>
2020-11-29 11:16:53 -0300

    Adicionado mapeamento para ligar usuario com tarefa

--- src/main/java/br/com/todolist/model/Usuario.java
+++ src/main/java/br/com/todolist/model/Usuario.java
@@ -1,10 +1,8 @@
 package br.com.todolist.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.List;
 
 @Entity(name = "usuario")
 public class Usuario implements Serializable {
@@ -17,4 +15,49 @@ public class Usuario implements Serializable {
 
     @Column(name = "nome", nullable = false, length = 50)
     private String nome;
+
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
+    private  List<ListaTarefa> listaTarefas;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Usuario other = (Usuario) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
 }
